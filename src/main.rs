7c0b8df63538ce4c9f5dde96fcf81730f0b8086e
jayden-dang <jayden@jaydendang.com>
2023-12-08 15:01:04 +0700

    chono: update axum version 7.0

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,19 @@
-use std::net::SocketAddr;
-
-use axum::{routing, Router, Server};
+use axum::{routing, Router};
 use tracing::info;
 
 #[tokio::main]
 async fn main() {
   tracing_subscriber::fmt::init();
 
-  let app = Router::new().route("/", routing::get(|| async { "Hello!" }));
-  let addr: SocketAddr = SocketAddr::from(([0, 0, 0, 0], 8888));
+  let app = Router::new().route("/", routing::get(hello_world));
+
+  let addr = "0.0.0.0:8888";
+  let listener = tokio::net::TcpListener::bind(addr).await.unwrap();
 
   info!("Server is running on port: {}", addr);
-  Server::bind(&addr).serve(app.into_make_service()).await.unwrap();
+  axum::serve(listener, app).await.unwrap()
+}
+
+async fn hello_world() -> &'static str {
+  "Hello, World!"
 }
