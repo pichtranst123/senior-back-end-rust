b4e14bac04dc93fe7692dd8c697196b29903390a
jayden-dang <jayden@jaydendang.com>
2023-12-08 16:48:57 +0700

    chono: update README.org

--- crates/jayden_bin/src/main.rs
+++ crates/jayden_bin/src/main.rs
@@ -6,15 +6,13 @@ use tracing::info;
 
 #[tokio::main]
 async fn main() {
-  let subscriber = tracing_subscriber::fmt::Subscriber::builder().with_max_level(tracing::Level::TRACE).finish();
+  let subscriber = tracing_subscriber::fmt::Subscriber::builder().with_max_level(tracing::Level::INFO).finish();
   tracing::subscriber::set_global_default(subscriber).expect("Cannot setting subscriber global");
 
-  info!("Loading Environment");
+  info!("Loading Environment & Migrating Database!");
   dotenv().ok();
 
   let config = Config::from_env().map_err(|e| tracing::error!("Loading environment Error: {}", e.to_string())).unwrap();
-
-  let addr = format!("0.0.0.0:{}", config.web.addr);
 
   // Region:     --- database
   let pool = initialized_database(&config.postgres.dsn, config.postgres.max_conns).await;
@@ -23,8 +21,7 @@ async fn main() {
   // End-Region: --- database
 
   let app = Router::new().route("/", routing::get(hello_world)).with_state(context);
-
-  let listener = tokio::net::TcpListener::bind(addr).await.unwrap();
+  let listener = tokio::net::TcpListener::bind(format!("0.0.0.0:{}", config.web.addr)).await.unwrap();
 
   info!("Server is running on port: {}", config.web.addr);
   axum::serve(listener, app).await.unwrap()
